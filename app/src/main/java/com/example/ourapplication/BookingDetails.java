dc303f0bbcd230e9553a4145e9099ffc6666b4ce
Jomana <jomanajihad6@gmail.com>
2021-04-22 01:52:00 +0300

    New : Booking Details Activity

---
+++ app/src/main/java/com/example/ourapplication/BookingDetails.java
@@ -0,0 +1,14 @@
+package com.example.ourapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_details);
+    }
+}
